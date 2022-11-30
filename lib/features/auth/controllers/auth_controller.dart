785d2fa1260019efd0aee0b68aea2031ee60d9f1
Olamide <muhammadolammi@gmail.com>
2022-11-30 22:08:22 +0100

    Working on Firebase(Daily Check in)

---
+++ lib/features/auth/controllers/auth_controller.dart
@@ -0,0 +1,12 @@
+import 'package:reddit_clone/features/auth/repositories/auth_repository.dart';
+
+class AuthConroller {
+  final AuthRepository _authRepository;
+
+  AuthConroller({required AuthRepository authRepository})
+      : _authRepository = authRepository;
+
+  void SignInWithGoogle() {
+    _authRepository.SignInWithGooge();
+  }
+}

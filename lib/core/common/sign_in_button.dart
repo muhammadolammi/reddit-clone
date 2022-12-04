070f5ee77eedff5a8b16e9ce347dc569cc8df5d2
Olamide <muhammadolammi@gmail.com>
2022-12-04 18:40:57 +0100

    resuming work

---
+++ lib/core/common/sign_in_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:reddit_clone/core/constant/constant.dart';
+import 'package:reddit_clone/theme/pallete.dart';
+
+class SignInButton extends StatelessWidget {
+  const SignInButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: ElevatedButton.icon(
+        onPressed: () {},
+        icon: Image.asset(
+          Constant.googleImagePath,
+          width: 35,
+        ),
+        label: const Text(
+          'Continue with Google',
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Pallete.greyColor,
+            minimumSize: const Size(double.infinity, 50),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            )),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_clone/features/auth/screens/login.dart';
 import 'package:reddit_clone/theme/pallete.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -12,9 +19,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Reddir Clone',
       theme: Pallete.darkModeAppTheme,
-      home: const SizedBox(),
+      home: const LoginScreen(),
     );
   }
 }
